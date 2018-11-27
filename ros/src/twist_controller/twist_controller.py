a2ba3c45f745025278660546dce285d4aec9b7e1
suraj lal <laljarus@gmail.com>
2018-11-27 07:21:55 +0000

    u

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -1,4 +1,4 @@
-
+    
 GAS_DENSITY = 2.858
 ONE_MPH = 0.44704
 
@@ -27,20 +27,18 @@ class Controller(object):
 
         self.tau_vel_filt 			= 0.3
         self.ts 				= 1/20.0  # sample time
-	
-	self.min_throttle 			= decel_limit
-	self.max_throttle			= 0.3
+        self.min_throttle 			= decel_limit
+        self.max_throttle			= 0.3
+        self.steering				= 0
 
         self.vel_low_pass_filt  = LowPassFilter(self.tau_vel_filt,self.ts)
-	self.pid_lon 		= PID(self.kp_lon,self.ki_lon,self.kd_lon,self.min_throttle,self.max_throttle)
+        self.pid_lon 		= PID(self.kp_lon,self.ki_lon,self.kd_lon,self.min_throttle,self.max_throttle)
         self.pid_lat            = PID(self.kp_lat,self.ki_lat,self.kd_lat,-self.max_steer_angle,self.max_steer_angle)
 
-	self.last_time 		= 0	
-	self.decel_limit 	= decel_limit
-	self.vehicle_mass	= vehicle_mass
-	self.wheel_radius 	= wheel_radius
-
-
+        self.last_time 		= 0	
+        self.decel_limit 	= decel_limit
+        self.vehicle_mass	= vehicle_mass
+        self.wheel_radius 	= wheel_radius
 
 
         self.YawController = YawController(self.wheel_base,self.steer_ratio,self.min_speed,self.max_lat_acc,self.max_steer_angle)
@@ -50,33 +48,32 @@ class Controller(object):
         
         act_x_dot = self.vel_low_pass_filt.filt(act_x_dot)
 
-        steer = self.YawController.get_steering(cmd_x_dot,cmd_yaw_rate,act_x_dot)
+        self.steer = self.YawController.get_steering(cmd_x_dot,cmd_yaw_rate,act_x_dot)
 
-	if not dbw_enabled:
-		self.pid_lon.reset()
-		return 0.,0.,0.
+        if not dbw_enabd:
+		    self.pid_lon.ret()
+		    return 00.
 
-	speed_error = cmd_x_dot - act_x_dot
-	current_time = rospy.get_time()
-	sample_time = current_time - self.last_time
+	    speed_error = cmd_x_doct_x_dot
+	    current_time py.get_time()
+	    sample_time = currentself.last_time
 
-        yaw_rate_err = cmd_yaw_rate - act_yaw_rate
+        yaw_rate_erd_yaw_rate - act_yaw_rate
 	
-	throttle = self.pid_lon.step(speed_error,sample_time,0.)
-	brake = 0.
+	    throttle = self.pidstep(spr,sample_time,0.)
+	    brake = 0.
 
-        steer = self.pid_lat.step(yaw_rate_err,sample_time,steer)
+        #steer =step(yaw_rate_err,sample_timsteer)
         
-	
-	if cmd_x_dot == 0. and act_x_dot < 0.1:
-		throttle = 0.
-		brake = 400.
-	elif throttle < 0. and speed_error < 0.:
-		throttle = 0.
-		decel = max(speed_error,self.decel_limit)
-		brake = abs(decel)*self.vehicle_mass*self.wheel_radius
+	    if cmd_x_d 0. and act_x_dot < 0.1:
+		    throte = 0.
+		    brake = 400.
+	    elif throttle < 0. and speed_err < 0.:
+		    throttle = 0.
+		    decel = max(speed_errorelf.decel_limit)
+		    brake = abs(decel)*self.vehicle_mass*self.wheel_radius
 		
 
 
         # Return throttle, brake, steer
-        return 0.1, 0., steer
+        return 0.2, 0., self.steer
